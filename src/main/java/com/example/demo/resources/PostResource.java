7daf1a8f00f9d4387d84d0b1a8da5170367ec279
Danilo Alvim <danilo.alvim@hotmail.com>
2021-04-23 15:10:51 -0300

    Consulta simples com query methods

--- src/main/java/com/example/demo/resources/PostResource.java
+++ src/main/java/com/example/demo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.example.demo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Post;
+import com.example.demo.resources.util.URL;
 import com.example.demo.services.PostService;
 
 @RestController
@@ -18,6 +22,13 @@ public class PostResource {
 	private PostService service;
 	
 
+	@RequestMapping(value ="/titlesearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitleContaining(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
 	@RequestMapping(value ="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
